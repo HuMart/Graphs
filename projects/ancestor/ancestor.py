3a38036d93ede523d997de3b347e36b5e34ea123
HuMart <manya01@protonmail.com>
2019-10-08 17:29:56 -0400

    printing earliest ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -42,3 +42,15 @@ def earliest_ancestor(ancestors, starting_node):
     ancestor = dfs(starting_node, family)
     return ancestor
 
+
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+                  (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+print(earliest_ancestor(test_ancestors, 1))
+
+print(earliest_ancestor(test_ancestors, 7))
+
+print(earliest_ancestor(test_ancestors, 9))
+
+print(earliest_ancestor(test_ancestors, 6))
+
